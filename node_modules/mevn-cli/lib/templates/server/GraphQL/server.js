6e912f1a460479d2bb6c0c22bda7d08fd9aab66c
Riccardo <riccardofaninfanin@gmail.com>
2024-11-05 10:40:41 +0100

    aggiunta struttura base express + vue

---
+++ node_modules/mevn-cli/lib/templates/server/GraphQL/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphQLHttp = require('express-graphql');
+const schema = require('./graphql/schema');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphQLHttp({
+    schema,
+    graphiql: true,
+  }),
+);
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Listening on localhost:${PORT}/graphql`);
+});
