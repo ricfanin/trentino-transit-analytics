6050dd030fc5832d352f440aed9efc7f8bf478ab
Matwy <mattiabasket2004@gmail.com>
2025-02-10 19:24:39 +0100

    github action 5

--- server/config/config.js
+++ server/config/config.js
@@ -8,8 +8,8 @@ const envVarsSchema = Joi.object()
     .keys({
         NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
         PORT: Joi.number().default(3000),
-        MONGODB_URL: Joi.string().required().description('Mongo DB url'),
-        JWT_SECRET: Joi.string().required().description('JWT secret key'),
+        MONGODB_URL: Joi.string().description('Mongo DB url'),
+        JWT_SECRET: Joi.string().default('secret').description('JWT secret key'),
         JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
         JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
         JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
